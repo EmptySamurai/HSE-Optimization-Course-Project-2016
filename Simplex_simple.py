724057bf330adb7120eea579ddbfb5679240ab5b
beaal <deadspring@yandex.ru>
2016-12-28 01:00:53 +0300

    Симплекс метод с помощью scipy

---
+++ Simplex_simple.py
@@ -0,0 +1,19 @@
+from minimizer import MinimizerMixin
+
+
+class SimplexSimple(MinimizerMixin):
+
+    def solve_klee_minty(self, n, maxiter=1000):
+        """
+        :param n: number of varuable in Klee-Minty cube
+        :param maxiter: max number of iterations
+        :return: map same as scipy.optimize.OptimizeResult
+        """
+        # завтра я все тут напишу...
+        return {
+            "x": [0, 0, 0],  # The solution of the optimization
+            "fun": 0,  # Value of objective function
+            "nit": 0,  # Number of algorithm iterations
+            "success": True,  # If algorithm succeed (number of iterations doesn't exceed max number),
+            "nfev": 0,  # Number of evaluations of the objective functions
+        }
