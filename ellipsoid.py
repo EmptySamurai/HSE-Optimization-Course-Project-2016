41a56068770879d57acda1c5f8a170f7b56f4d8a
imelnik93 <i.melnik93@gmail.com>
2016-12-29 08:36:42 +0300

    ellipsoid

--- ellipsoid.py
+++ ellipsoid.py
@@ -27,18 +27,19 @@ def ellipsoid(n, maxiter, A, b, c):
         if feas_test == len(feas_test)*[True]:
             sgn = c*subgr_norm(c, P)  # subgrad, normalized
             fbest = np.dot(c, x)  # update the best solution
-            alpha = (np.dot(c, x)-fbest)*subgr_norm(c, P)  # shift param
+            alpha = (np.dot(c, x)-fbest)*subgr_norm(c, P)  # set the shift param
         else:
-            uf = feas_test.index(False)
-            sgn = A[uf]*subgr_norm(c, P)
-            alpha = np.dot(A[uf], x)*subgr_norm(A[uf], P)
-        x_new = x-(1+n*alpha)/(1+n)*np.dot(P, sgn)
-        if np.norm(x-x_new) < 0.01:
-            return('ok')
+            uf = feas_test.index(False)  # the first violated constraint
+            sgn = A[uf]*subgr_norm(c, P)  # subgrad, normalized
+            alpha = np.dot(A[uf], x)*subgr_norm(A[uf], P)  # set the shift param
+        x_new = x-(1+n*alpha)/(1+n)*np.dot(P, sgn)  # set the new x
+        if np.norm(x-x_new) < 0.01:  # condition on diff norm: if x stopped moving, we are fine
+            return k
+            break
         else:
-            x = x_new
-            pgmult = np.dot(np.dot(P, np.outer(sgn, sgn)), P)
-            P = n**2/(n**2-1)*(1-alpha**2)*(P-2*(1+n*alpha)/((1+n)*(1+alpha))*pgmult)
+            x = x_new  # update x for next runs
+            pgmult = np.dot(np.dot(P, np.outer(sgn, sgn)), P)  # matrix mult for ellipse
+            P = n**2/(n**2-1)*(1-alpha**2)*(P-2*(1+n*alpha)/((1+n)*(1+alpha))*pgmult)  # new ellipse shape
             k += 1
 
 
